276ac06fb9b9b2f6f9c85a976eb21091850db549
rumeysametin <rumeysametin1166@gmail.com>
2021-09-09 18:37:33 +0300

    first commit

---
+++ player.py
@@ -0,0 +1,13 @@
+name = input("player name: ")
+surname = input("player surname: ")
+team = input("player team: ")
+info = [name, surname, team]
+print( "player name: {}\nplayer surname: {}\nplayer team: {}\n".format(info[0], info[1], info[2]) )
+
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+
+çarpım = a * b * c
+
+print("{} x {} x {} = {} dir".format(a,b,c,çarpım))
