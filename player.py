841493f6e544f4d086331a136b4546a8248769d7
rumeysametin <rumeysametin1166@gmail.com>
2021-09-15 00:34:38 +0300

    function

--- player.py
+++ player.py
@@ -146,3 +146,14 @@ while True:
       print(number,"is a prime number")
     else:
       print(number,"isn't a prime number")
+
+def pisagor():
+    pisagor_list = []
+    for i in range(1,101):
+        for j in range(1,101):
+            c = (i ** 2 + j ** 2) ** 0.5
+            if (c == int(c) ):
+                pisagor_list.append((i,j,int(c)))
+    return pisagor_list
+for i in pisagor():
+    print(i)
