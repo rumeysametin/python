235bc4df9f07bea536a919f2b169a2408a9b5780
rumeysametin <rumeysametin1166@gmail.com>
2021-11-11 18:41:07 +0300

    templates

--- blog.py
+++ blog.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Home Page"
-@app.route("/about")
-def about():
-    return "About Me"
+    article = dict()
+    article["title"] = "Test"
+    article["body"] = "Test 123"
+    article["author"] = "Joseph Addison"
+    number = 10
+    return render_template("index.html", n1 = number, article = article)
 if __name__ == "__main__":
     app.run(debug=True)
