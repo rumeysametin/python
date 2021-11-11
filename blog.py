f0bb217dea289bee44644e00c2fa8fa829fba4b4
rumeysametin <rumeysametin1166@gmail.com>
2021-11-11 18:56:38 +0300

    template inheritance and blocks

--- blog.py
+++ blog.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    article = dict()
-    article["title"] = "Test"
-    article["body"] = "Test 123"
-    article["author"] = "Joseph Addison"
-    number = 10
-    return render_template("index.html", n1 = number, article = article)
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
