1d9ab8f08db5b62995599c9adfc6f6ed8be08141
rumeysametin <rumeysametin1166@gmail.com>
2021-11-13 17:11:40 +0300

    Mysql configuration with Flask

--- blog.py
+++ blog.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "blog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 @app.route("/")
 def index():
-    numbers =  [1,2,3,4,5]
-    return render_template("index.html", numbers = numbers)
+    return render_template("index.html")
 @app.route("/about")
 def about():
     return render_template("about.html")
+@app.route("/article/<string:id>")
+def detail(id):
+    return "Article id:" + id
 if __name__ == "__main__":
     app.run(debug=True)
