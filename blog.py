c61a375878a0cd4e83f3fccfc77e906f4d1e9440
rumeysametin <rumeysametin1166@gmail.com>
2021-11-16 15:47:03 +0300

    register form

--- blog.py
+++ blog.py
@@ -3,6 +3,16 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
+class RegisterForm(Form):
+    name = StringField("name", validators=[validators.Length(min= 4, max=25)])
+    username = StringField("username", validators=[validators.Length(min= 5, max=35)])
+    email = StringField("email", validators=[validators.Length(min=10, max=30)])
+    password = PasswordField("password", validators=[
+        validators.DataRequired(message="please set password"),
+        validators.EqualTo(fieldname= "confirm", message= "your password doesn't match")
+    ])
+    confirm = PasswordField("verify password")
+
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -20,5 +30,13 @@ def about():
 @app.route("/article/<string:id>")
 def detail(id):
     return "Article id:" + id
+
+@app.route("/register")
+def register():
+    form = RegisterForm(request.form)
+    if request.method == "POST":
+         return redirect(url_for("index"))
+    else:
+        return render_template("register.html", form = form)
 if __name__ == "__main__":
     app.run(debug=True)
